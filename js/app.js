60e0c909e83f8212f05659344a199f821105dc67
Alex <alexnschenck@gmail.com>
2014-10-28 09:42:16 -0700

    first commit 10/28/14

--- js/app.js
+++ js/app.js
@@ -4,3 +4,38 @@
 
 "use strict";
 
+// select all navigation links
+var allNavLinks = $('nav a');
+var allImgs = $('img');
+var allSections = $('section');
+
+function onReady() {
+	$('nav').slideUp();
+	$('nav').slideDown();
+	allSections.hide();
+} //onReady
+
+allImgs.hover(function() {
+	// console.log($(this));
+	$(this).addClass('awesomesauce');
+}); // allImgs onClick
+
+allImgs.mouseout(function() {
+	$(this).removeClass('awesomesauce');
+});
+
+allNavLinks.click(function() {
+	// the cooler jQuery this
+	console.log($(this));
+
+	// plain version
+	console.log(this);
+
+	// acts on the jQuery object containing this element
+	//allSections.fadeIn(6000);
+
+	allSections.hide();
+	$($(this).attr('href')).fadeToggle(1000);
+}); // allNavLinks onClick
+
+$(onReady);
